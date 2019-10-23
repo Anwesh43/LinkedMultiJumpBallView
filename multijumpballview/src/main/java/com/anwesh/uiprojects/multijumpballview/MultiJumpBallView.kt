a8764c1386a8199689ae002291eebd97a398c437
Anwesh43 <anweshthecool0@gmail.com>
2019-10-23 06:32:49 +0530

    adding Int and Float inverse methods

--- multijumpballview/src/main/java/com/anwesh/uiprojects/multijumpballview/MultiJumpBallView.kt
+++ multijumpballview/src/main/java/com/anwesh/uiprojects/multijumpballview/MultiJumpBallView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.multijumpballview
 /**
  * Created by anweshmishra on 23/10/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val balls : Int = 3
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#f44336")
+val backColor : Int = Color.parseColor("#BDBDBD")
