7ca4787bd8483616252ce3dd29d53e32e10014a3
Anwesh43 <anweshthecool0@gmail.com>
2019-10-23 06:48:27 +0530

    adding MultiJumpBallView class

--- multijumpballview/src/main/java/com/anwesh/uiprojects/multijumpballview/MultiJumpBallView.kt
+++ multijumpballview/src/main/java/com/anwesh/uiprojects/multijumpballview/MultiJumpBallView.kt
@@ -51,3 +51,21 @@ fun Canvas.drawMJBNode(i : Int, scale : Float, paint : Paint) {
     drawMultiJumpBall(scale, size, h, paint)
     restore()
 }
+
+class MultiJumpBallView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
